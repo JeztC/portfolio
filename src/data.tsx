260c123bd2d996ebe2e4acaa7d48a07c84702b5b
JeztC <dagreenrs@gmail.com>
2023-10-22 18:20:03 +0300

    feat: add data files to make life more convinient.

---
+++ src/data.tsx
@@ -0,0 +1,28 @@
+const skillsList = [
+    { title: 'JavaScript', value: 75 },
+]
+
+const education = [
+    {
+        id: 0,
+        school: 'University of Helsinki',
+        schoolLogo: 'https://www.sttinfo.fi/data/images/00382/395bc891-fb8c-47c3-a253-6a2b19670133-w_300_h_100.jpg',
+        links: {
+            website: 'https://www.helsinki.fi/fi',
+            facebook: 'https://www.facebook.com/',
+            instagram: 'https://www.instagram.com/',
+        },
+        courses: [
+            {
+                courseName: 'FullStackOpen',
+                courseLink: 'https://fullstackopen.com',
+                schoolLink: 'https://www.helsinki.fi/fi',
+                schoolName: 'University of Helsinki',
+                dateFinished: '1.1.1970',
+                grade: 5,
+            },
+        ]
+    }
+]
+
+export { skillsList, education }
